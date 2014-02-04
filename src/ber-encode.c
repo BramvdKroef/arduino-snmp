89be0c17ffb56de1adcf1be631ecddc0ea4e81fa
Bram van der Kroef <bram@vanderkroef.net>
2014-02-04 15:09:43 -0600

    Replaced byte with uint8_t and added const to constant parameters.

--- src/ber-encode.c
+++ src/ber-encode.c
@@ -1,17 +1,17 @@
 #include "ber-encode.h"
 
-size_t ber_encode_calcHeadLen(size_t data_length) {
+size_t ber_encode_calcHeadLen(const size_t data_length) {
   return 2;
 }
 
-size_t ber_encode_length(byte* data, size_t length) {
+size_t ber_encode_length(uint8_t* data, const size_t length) {
   data[0] = (uint8_t)length;
   return 1;
 }
 
-size_t ber_encode_integer(byte* data, uint8_t type, int32_t value) {
-  size_t len, offset;
-  int i;
+size_t ber_encode_integer(uint8_t* data, const uint8_t type, const int32_t value) {
+  size_t len, offset, i;
+  uint8_t* tmp = &value;
 
   data[0] = type;
 
@@ -20,15 +20,14 @@ size_t ber_encode_integer(byte* data, uint8_t type, int32_t value) {
   offset = 1 + ber_encode_length(data + 1, len);
     
   // copy the bytes from value to data backwards
-  for (i = len - 1; i >= 0; i--) {
-    data[offset + i] = value & 0xFF;
-    value >>= 8;
+  for (i = 0; i < len; i++) {
+    data[offset + i] = tmp[len - (i + 1)];
   }
   
   return offset + len;
 }
 
-size_t ber_encode_integer_size(int32_t value) {
+size_t ber_encode_integer_size(const int32_t value) {
   size_t len = 1;
   int32_t tmp = value;
   tmp = tmp >> 8;
@@ -40,8 +39,8 @@ size_t ber_encode_integer_size(int32_t value) {
   return len;
 }
 
-size_t ber_encode_octets(uint8_t* data, uint8_t type, uint8_t* value,
-                         size_t len) {
+size_t ber_encode_octets(uint8_t* data, const uint8_t type, const uint8_t* value,
+                         const size_t len) {
   size_t offset;
   data[0] = type;
   offset = 1 + ber_encode_length(data + 1, len);
@@ -49,21 +48,21 @@ size_t ber_encode_octets(uint8_t* data, uint8_t type, uint8_t* value,
   return offset + len;
 }
 
-size_t ber_encode_str(uint8_t* data, uint8_t type, const char* value) {
+size_t ber_encode_str(uint8_t* data, const uint8_t type, const char* value) {
   return ber_encode_octets(data, type, (uint8_t*)value, strlen(value));
 }
 
-size_t ber_encode_object (byte* data, oid* oid) {
+size_t ber_encode_object (uint8_t* data, const oid* oid) {
   return ber_encode_octets(data, BER_OBJECT, oid->data, oid->len);
 }
 
-size_t ber_encode_null(byte* data) {
+size_t ber_encode_null(uint8_t* data) {
   data[0] = BER_NULL;
   data[1] = 0;
   return 2;
 }
 
-size_t ber_encode_sequence(byte* data, uint8_t type, size_t size) {
+size_t ber_encode_sequence(uint8_t* data, const uint8_t type, const size_t size) {
   data[0] = type;
   return 1 + ber_encode_length(data + 1, size);
 }
--- src/ber-encode.h
+++ src/ber-encode.h
@@ -4,15 +4,15 @@
 #include "Arduino.h"
 #include "ber.h"
 
-size_t ber_encode_calcHeadLen(size_t data_length);
-size_t ber_encode_length(byte* data, size_t length);
-size_t ber_encode_integer(byte* data, uint8_t type, int32_t value);
-size_t ber_encode_integer_size(int32_t value);
-size_t ber_encode_octets(uint8_t* data, uint8_t type, uint8_t* value,
-                         size_t len);
-size_t ber_encode_str(uint8_t* data, uint8_t type, const char* value);
-size_t ber_encode_object (byte* data, oid* oid);
-size_t ber_encode_null(byte* data);
-size_t ber_encode_sequence(byte* data, uint8_t type, size_t size);
+size_t ber_encode_calcHeadLen(const size_t data_length);
+size_t ber_encode_length(uint8_t* data, const size_t length);
+size_t ber_encode_integer(uint8_t* data, const uint8_t type, const int32_t value);
+size_t ber_encode_integer_size(const int32_t value);
+size_t ber_encode_octets(uint8_t* data, const uint8_t type, const uint8_t* value,
+                         const size_t len);
+size_t ber_encode_str(uint8_t* data, const uint8_t type, const char* value);
+size_t ber_encode_object (uint8_t* data, const oid* oid);
+size_t ber_encode_null(uint8_t* data);
+size_t ber_encode_sequence(uint8_t* data, const uint8_t type, const size_t size);
 
 #endif
